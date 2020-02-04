d69d5c2ebd0006ef2314b04bb51eec723a0a497e
qkrtkddn <qkrtkddn76@naver.com>
2020-02-04 15:43:57 +0900

    login,signUP

---
+++ src/Styles/Theme.js
@@ -0,0 +1,20 @@
+const BOX_BORDER ="1px soloid #e6e6e6";
+const BORDER_RADIUS="4px";
+
+
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor:"#999",
+  lightGreyColor:"#c7c7c7",
+  blueColor: "#3897f0",
+  redColor:"#ED4956",
+  darkBlueColor: "#003569",
+  boxBorder: "1px soloid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `border:${BOX_BORDER};
+             border-radius: ${BORDER_RADIUS};
+              background-color:white;
+            `
+};
